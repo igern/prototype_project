845bcf7d1c7450cd7716ebb41c9b2103e5a79505
jonas <jones16511@hotmail.com>
2021-06-06 11:04:55 +0200

    added first version of MultiSlider

--- lib/pages/macro_meal_multi_drag_line_page.dart
+++ lib/pages/macro_meal_multi_drag_line_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class MacroMealMultiDragLinePage extends StatelessWidget {
@@ -5,6 +7,163 @@ class MacroMealMultiDragLinePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: MultiSlider(),
+      ),
+    );
+  }
+}
+
+class MultiSlider extends StatefulWidget {
+  MultiSlider({Key? key}) : super(key: key);
+
+  @override
+  _MultiSliderState createState() => _MultiSliderState();
+}
+
+class _MultiSliderState extends State<MultiSlider> {
+  late List<double> _thumbXPositions;
+
+  int _currentThumbDragIndex = 0;
+  int _divisions = 100;
+
+  List<double> get _steps =>
+      List.generate(_divisions + 1, (index) => 100 / _divisions * index);
+
+  @override
+  void initState() {
+    super.initState();
+    _thumbXPositions = [0.5, 0.8];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onHorizontalDragStart: (DragStartDetails details) {
+        _currentThumbDragIndex =
+            findClosestThumbIndex(details.localPosition.dx);
+
+        updateThumbPosition(details.localPosition.dx);
+      },
+      onHorizontalDragUpdate: (DragUpdateDetails details) {
+        updateThumbPosition(details.localPosition.dx);
+      },
+      child: Container(
+          width: 600,
+          height: 30,
+          child: CustomPaint(
+              painter: MultiSliderPainter(
+                  _thumbXPositions[0], _thumbXPositions[1]))),
+    );
+  }
+
+  void updateThumbPosition(double dx) {
+    if (_currentThumbDragIndex == 0) {
+      if (_thumbXPositions[_currentThumbDragIndex] <= _thumbXPositions[1]) {
+        setState(() {
+          _thumbXPositions[_currentThumbDragIndex] = closestDivision(
+              min(dx / 600, _thumbXPositions[1] - 100 / _divisions / 100));
+        });
+      }
+    } else if (_currentThumbDragIndex == _thumbXPositions.length - 1) {
+      if (_thumbXPositions[_currentThumbDragIndex] >=
+          _thumbXPositions[_currentThumbDragIndex - 1]) {
+        setState(() {
+          _thumbXPositions[_currentThumbDragIndex] = closestDivision(max(
+              dx / 600,
+              _thumbXPositions[_currentThumbDragIndex - 1] +
+                  100 / _divisions / 100));
+        });
+      }
+    }
+  }
+
+  double closestDivision(double x) {
+    double closest = -1;
+    double closestDistance = double.maxFinite;
+    for (int i = 0; i < _steps.length; i++) {
+      double distance = (x - _steps[i] / 100).abs();
+      if (distance < closestDistance) {
+        closest = _steps[i] / 100;
+        closestDistance = distance;
+      }
+    }
+    return closest;
+  }
+
+  int findClosestThumbIndex(double dx) {
+    int closest = -1;
+    double closestDistance = double.maxFinite;
+    for (int i = 0; i < _thumbXPositions.length; i++) {
+      double distance = (dx - _thumbXPositions[i] * 600).abs();
+      if (distance < closestDistance) {
+        closest = i;
+        closestDistance = distance;
+      }
+    }
+    return closest;
+  }
+}
+
+class MultiSliderPainter extends CustomPainter {
+  final double firstThumb;
+  final double secondThumb;
+
+  MultiSliderPainter(this.firstThumb, this.secondThumb);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var centerY = size.height / 2;
+
+    var thumbWidth = 40.0;
+    var thumbHeight = 30.0;
+
+    var firstThumbRect = Rect.fromCenter(
+        center: Offset(size.width * firstThumb, centerY),
+        width: thumbWidth,
+        height: thumbHeight);
+
+    var secondThumbRect = Rect.fromCenter(
+        center: Offset(size.width * secondThumb, centerY),
+        width: thumbWidth,
+        height: thumbHeight);
+
+    var firstSectionLineBrush = Paint()
+      ..color = Colors.red
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 20;
+
+    var secondSectionLineBrush = Paint()
+      ..color = Colors.green
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 20;
+
+    var thirdSectionLineBrush = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 20;
+
+    var thumbFill = Paint()..color = Colors.black;
+
+    canvas.drawLine(Offset(0, centerY),
+        Offset(size.width * firstThumb, centerY), firstSectionLineBrush);
+    canvas.drawLine(Offset(size.width * firstThumb, centerY),
+        Offset(size.width * secondThumb, centerY), secondSectionLineBrush);
+    canvas.drawLine(Offset(size.width * secondThumb, centerY),
+        Offset(size.width, centerY), thirdSectionLineBrush);
+
+    canvas.drawRRect(
+        RRect.fromRectAndRadius(firstThumbRect, Radius.circular(10)),
+        thumbFill);
+    canvas.drawRRect(
+        RRect.fromRectAndRadius(secondThumbRect, Radius.circular(10)),
+        thumbFill);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
--- lib/pages/macro_meal_single_drag_line_page.dart
+++ lib/pages/macro_meal_single_drag_line_page.dart
@@ -1,10 +1,130 @@
 import 'package:flutter/material.dart';
 
-class MacroMealSingleDragLinePage extends StatelessWidget {
+class MacroMealSingleDragLinePage extends StatefulWidget {
   const MacroMealSingleDragLinePage({Key? key}) : super(key: key);
 
   @override
+  _MacroMealSingleDragLinePageState createState() =>
+      _MacroMealSingleDragLinePageState();
+}
+
+class _MacroMealSingleDragLinePageState
+    extends State<MacroMealSingleDragLinePage> {
+  double _value = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SliderTheme(
+        data: SliderTheme.of(context).copyWith(
+            activeTrackColor: Colors.red[700],
+            inactiveTrackColor: Colors.red[100],
+            trackShape: RoundedRectSliderTrackShape(),
+            trackHeight: 4.0,
+            thumbShape: CustomSliderThumbRect(
+              thumbHeight: 60,
+              thumbRadius: 20,
+              min: 0,
+              max: 100,
+            ),
+            thumbColor: Colors.redAccent,
+            // overlayColor: Colors.red.withAlpha(32),
+            // overlayShape: RoundSliderOverlayShape(overlayRadius: 28.0),
+            // tickMarkShape: RoundSliderTickMarkShape(),
+            // activeTickMarkColor: Colors.red[700],
+            // inactiveTickMarkColor: Colors.red[100],
+            showValueIndicator: ShowValueIndicator.never
+            // valueIndicatorShape: PaddleSliderValueIndicatorShape(),
+            // valueIndicatorColor: Colors.redAccent,
+            // valueIndicatorTextStyle: TextStyle(
+            //   color: Colors.white,
+            // ),
+            ),
+        child: Slider(
+          value: _value,
+          min: 0,
+          max: 100,
+          divisions: 10,
+          label: '$_value',
+          onChanged: (value) {
+            setState(
+              () {
+                _value = value;
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class CustomSliderThumbRect extends SliderComponentShape {
+  final double thumbRadius;
+  final double thumbHeight;
+  final int min;
+  final int max;
+
+  const CustomSliderThumbRect({
+    required this.thumbRadius,
+    required this.thumbHeight,
+    required this.min,
+    required this.max,
+  });
+
+  @override
+  Size getPreferredSize(bool isEnabled, bool isDiscrete) {
+    return Size.fromRadius(thumbRadius);
+  }
+
+  @override
+  void paint(
+    PaintingContext context,
+    Offset center, {
+    Animation<double>? activationAnimation,
+    Animation<double>? enableAnimation,
+    bool? isDiscrete,
+    TextPainter? labelPainter,
+    RenderBox? parentBox,
+    SliderThemeData? sliderTheme,
+    TextDirection? textDirection,
+    double? value,
+    double? textScaleFactor,
+    Size? sizeWithOverflow,
+  }) {
+    final Canvas canvas = context.canvas;
+
+    final rRect = RRect.fromRectAndRadius(
+      Rect.fromCenter(
+          center: center, width: thumbHeight * 1.2, height: thumbHeight * .6),
+      Radius.circular(thumbRadius * .4),
+    );
+
+    final paint = Paint()
+      ..color = sliderTheme!.activeTrackColor! //Thumb Background Color
+      ..style = PaintingStyle.fill;
+
+    TextSpan span = new TextSpan(
+        style: new TextStyle(
+            fontSize: thumbHeight * .3,
+            fontWeight: FontWeight.w700,
+            color: sliderTheme.thumbColor,
+            height: 1),
+        text: '${getValue(value!)}');
+    TextPainter tp = new TextPainter(
+        text: span,
+        textAlign: TextAlign.left,
+        textDirection: TextDirection.ltr);
+    tp.layout();
+    Offset textCenter =
+        Offset(center.dx - (tp.width / 2), center.dy - (tp.height / 2));
+
+    canvas.drawRRect(rRect, paint);
+
+    tp.paint(canvas, textCenter);
+  }
+
+  String getValue(double value) {
+    return (min + (max - min) * value).round().toString();
   }
 }
